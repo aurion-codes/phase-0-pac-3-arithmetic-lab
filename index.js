164ce9921914da0701e1ab681fbfaf3461a2ea07
AurionHoward <aurionhoward@gmail.com>
2021-09-09 00:30:05 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function add(a,b) {
+    return (a+b); 
+}
+
+function subtract(a,b) {
+    return (a-b);
+}
+
+function multiply(a,b) {
+    return (a*b);
+}
+
+function divide(a,b)    {
+    return (a/b);
+}
+
+function increment(n){
+    n++
+    return (n);
+}
+
+function decrement(n){
+    n--
+    return(n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+
+function preserveDecimal(n){
+    parseFloat('2.222', 10)
+    return parseFloat(n);
+}
+
